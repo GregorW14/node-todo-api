19cc69c3b33bc0ed58ea89694c8b67f7ee066b67
GregorW14 <gregwhyte14@gmail.com>
2017-11-27 23:17:00 +0000

    User authentication for routes and Hashing passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
 
-var data = {
-   id: 10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
-// var message = 'I am user number 1';
+var hashedPassword = '$2a$10$nJLLw3kuBFquEvCueeKMgOC5z4XoNrEFwHRH30Hw6n5aeG0uCypE2';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//    id: 10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
+// // var message = 'I am user number 1';
 // var hash = SHA256(message).toString();
 //
 // console.log(`Message: ${message}`);
